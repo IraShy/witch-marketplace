778f580fb69ba76264414354f87da4a7ba17f8ea
Iryna Shymbor <i.shymbor@gmail.com>
2020-05-15 19:38:41 +1000

    created models for orders and order_products

--- db/migrate/20200515092634_create_orders.rb
+++ db/migrate/20200515092634_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.string :state
       t.references :user, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_014119) do
+ActiveRecord::Schema.define(version: 2020_05_15_080700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_014119) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
     t.bigint "user_id", null: false
+    t.boolean "sold", default: false, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
